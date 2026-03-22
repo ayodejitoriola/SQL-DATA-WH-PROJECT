fbed232b7dc87a4da73349064189e39ee945c6a5
Ayodeji Toriola <112258291+ayodejitoriola@users.noreply.github.com>
2026-03-22 15:24:05 +0000

    Initialize DataWarehouse database and schemas
    Create a new database 'DataWarehouse' with schemas.

---
+++ scripts/init_database.sql
@@ -0,0 +1,15 @@
+/*
+create screate and schemas
+*/
+-- Create database 'Datawarehouse'
+USE master;
+
+create database DataWarehouse;
+
+use DataWarehouse;
+
+CREATE SCHEMA bronze;
+
+CREATE SCHEMA silver;
+
+CREATE SCHEMA gold;
